cd3872e7ac2a28ea2134c09d4da55964a41faf26
Zuthen <winstonivo@gmail.com>
2020-06-15 22:08:34 +0200

    Player move animation PoC

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rigidbody;
+    public Animator animator;
     Vector2 movement;
 
     // Update is called once per frame
@@ -13,6 +14,10 @@ public class PlayerMovement : MonoBehaviour
     {
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
+
+       animator.SetFloat("Horizontal", movement.x);
+       animator.SetFloat("Vertical", movement.y);
+       animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
