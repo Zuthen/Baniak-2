89052fb5178a6d03a32e4de61e65b51a56a890ce
Zuthen <winstonivo@gmail.com>
2020-06-17 00:19:52 +0200

    Animation changes on character button click

---
+++ Assets/CharacterSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSlot : MonoBehaviour
+{
+    public Character character;
+
+    private void Start()
+    {
+        GetComponentInChildren<Image>().sprite = character.avatar;
+        GetComponentInChildren<Text>().text = character.characterName;
+    }
+
+    public void ChangeCharacter()
+    {
+        PlayerMovement player = FindObjectOfType<PlayerMovement>();
+        player.animator.runtimeAnimatorController = character.animator;
+    }
+}
