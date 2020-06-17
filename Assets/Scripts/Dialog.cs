8147d327761a0e994bbd9145202b1ee9cef77d30
Zuthen <winstonivo@gmail.com>
2020-06-17 21:03:26 +0200

    TextMeshPro Dialogue system template

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public Image avatarDisplay;
+
+
+    //public string[] sentences;
+    public Speaker[] speakers;
+    private int index;
+    public float typingSpeed;
+    public GameObject continueButton;
+
+    public Speaker[] Speakers { get => speakers; set => speakers = value; }
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == Speakers[index].sentence)
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        avatarDisplay.sprite = Speakers[index].avatar;
+        foreach (char letter in Speakers[index].sentence.ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+        if (index < Speakers.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Speaker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Speaker", menuName ="Speaker/Dialogue")]
+public class Speaker : ScriptableObject
+{
+    public string sentence;
+    public Sprite avatar;
+}
